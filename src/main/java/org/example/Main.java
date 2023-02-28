626c393b29703377eb4c351447920ee2db079c0b
Sziku <105989158+Sziku@users.noreply.github.com>
2023-02-28 16:41:07 +0100

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+//Van egy Trianglevalidator osztályunk.
+//az osztálynak van egy public boolean validateTriangle(int[] sides) ami vissza adja hogy
+//tömben kapott oldalhosszokbol összeálítható-e egy háromszög.
+//ha a tömben kapott tömb mérete nem három akkor false-al tér vissza.
+//a validálást a háromszög egyenlőtlenség szabálya alapján csinálja
+// https://hu.wikipedia.org/wiki/H%C3%A1romsz%C3%B6g-egyenl%C5%91tlens%C3%A9g
+
+
+//van egy kamera store ahol kamerák vannak.
+//public Camera suggestCameraByMegapixel()
+//vissza adja a legnagyobb felbontásu kamerát.
+//ha nem található ilyen kamera akkor NotExistingCameraException-t dob
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
